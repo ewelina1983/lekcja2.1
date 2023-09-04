7798d7801f8be38fd7c0912e593fd44fd9ea8500
ewelina.stasko83 <ewelina.stasko83@gmail.com>
2023-09-04 15:11:10 +0200

    monday

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const { Router } = require("express");
+/*const { Router } = require("express");
 const { save } = require("../save_json");
 let favouriteNumber = require("../number.json");
 const add = require("../add");
@@ -45,6 +45,62 @@ router.delete("/favNumber", (req, res) => {
   save(favouriteNumber);
   res.json({
     status: "success"
+  });
+});
+
+module.exports = router;*/
+
+
+const { Router } = require("express");
+const { save } = require("../save_json");
+let favouriteNumber = require("../number.json");
+const add = require("../add");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+const router = new Router();
+
+router.get("/sum/:number1/:number2", async (req, res) => {
+  let my_file = await s3.getObject({
+    Bucket: "cyclic-alive-pantyhose-tuna-eu-west-2",
+    Key: "number.json",
+  }).promise()
+  const favNumber = JSON.parse(my_file.Body)?.favouriteNumber;
+  const {number1, number2} = req.params;
+  if(number1 == null || number2 == null) {
+    res.status(400).send("Not proviede number");
+    return;
+  }
+  if(isNaN(parseInt(number1)) || isNaN(parseInt(number2))) {
+    res.status(400).send("Numbers needs to be integer");
+    return;
+  }
+  let result = add(parseInt(number1), parseInt(number2));
+  if(favNumber != null) {
+    result = add(result, favNumber )
+  }
+  res.json({
+    status: "success",
+    result: result,
+  });
+});
+
+router.post("/favNumber", async (req, res) => {
+  const {number} = req.body;
+  if(number == null ) {
+    res.status(400).send("Not provided number");
+    return;
+  }
+  if(isNaN(parseInt(number))) {
+    res.status(400).send("The number needs to be integer");
+    return;
+  }
+  await save({
+    favouriteNumber: number
+  });
+  res.json({
+    status: "success",
+    newFavouriteNumber: number,
   });
 });
 
